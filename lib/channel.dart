c3506cb1462ffefafe3d48f005da21a484aea0a3
Eliseu Codinhoto <zeucxb@gmail.com>
2019-12-19 02:17:27 -0300

    add aqueduct config

--- lib/channel.dart
+++ lib/channel.dart
@@ -3,17 +3,31 @@ import 'controllers/heroes_controller.dart';
 import 'heroes.dart';
 
 class HeroesChannel extends ApplicationChannel {
+  ManagedContext context;
+
   @override
   Future prepare() async {
     logger.onRecord.listen(
         (rec) => print("$rec ${rec.error ?? ""} ${rec.stackTrace ?? ""}"));
+
+    final config = HeroConfig(options.configurationFilePath);
+    final dataModel = ManagedDataModel.fromCurrentMirrorSystem();
+    final persistentStore = PostgreSQLPersistentStore.fromConnectionInfo(
+      config.database.username,
+      config.database.password,
+      config.database.host,
+      config.database.port,
+      config.database.databaseName,
+    );
+
+    context = ManagedContext(dataModel, persistentStore);
   }
 
   @override
   Controller get entryPoint {
     final router = Router();
 
-    router.route('/heroes/[:id]').link(() => HeroesController());
+    router.route('/heroes/[:id]').link(() => HeroesController(context));
 
     router.route("/example").linkFunction((request) async {
       return Response.ok({"key": "value"});
@@ -21,4 +35,10 @@ class HeroesChannel extends ApplicationChannel {
 
     return router;
   }
+}
+
+class HeroConfig extends Configuration {
+  HeroConfig(String path) : super.fromFile(File(path));
+
+  DatabaseConfiguration database;
 }
--- lib/controllers/heroes_controller.dart
+++ lib/controllers/heroes_controller.dart
@@ -1,29 +1,43 @@
 import 'package:heroes/heroes.dart';
+import 'package:heroes/models/hero_model.dart';
 
 class HeroesController extends ResourceController {
-  final _heroes = [
-    {'id': 11, 'name': 'Mr. Nice'},
-    {'id': 12, 'name': 'Narco'},
-    {'id': 13, 'name': 'Bombasto'},
-    {'id': 14, 'name': 'Celeritas'},
-    {'id': 15, 'name': 'Magneta'},
-    {'id': 16, 'name': 'Eliseu'},
-  ];
+  HeroesController(this.context);
+
+  final ManagedContext context;
 
   @Operation.get()
-  Future<Response> getAllHeroes() async {
-    return Response.ok(_heroes);
+  Future<Response> getAllHeroes({@Bind.query('name') String name}) async {
+    final heroQuery = Query<Hero>(context);
+
+    if (name != null) {
+      heroQuery.where((hero) => hero.name).contains(name, caseSensitive: false);
+    }
+
+    final heroes = await heroQuery.fetch();
+
+    return Response.ok(heroes);
   }
 
   @Operation.get('id')
-  Future<Response> getHeroByID() async {
-    final id = int.parse(request.path.variables['id']);
-    final hero =
-        _heroes.firstWhere((hero) => hero['id'] == id, orElse: () => null);
+  Future<Response> getHeroByID(@Bind.path('id') int id) async {
+    final heroQuery = Query<Hero>(context)
+      ..where((hero) => hero.id).equalTo(id);
+    final hero = await heroQuery.fetchOne();
+
     if (hero == null) {
-      return Response.notFound();
+      return Response.notFound(body: {});
     }
 
     return Response.ok(hero);
+  }
+
+  @Operation.post()
+  Future<Response> createHero(@Bind.body(ignore: ['id']) Hero inputHero) async {
+    final query = Query<Hero>(context)..values = inputHero;
+
+    final insertedHero = await query.insert();
+
+    return Response.ok(insertedHero);
   }
 }
